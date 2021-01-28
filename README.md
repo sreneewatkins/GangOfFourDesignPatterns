f3c9aff16729147e86413acf30d4e1f055e65585
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2021-01-28 10:14:01 -0600

    Initial Commit - Creational

---
+++ README.md
@@ -0,0 +1,9 @@
+#Java Design Patterns
+###Creational Design Patterns
+
+1. *Builder*: Builder design pattern is an alternative way to construct complex objects and should be used only when we want to build different types of immutable objects using same object building process.
+2. *Prototype*: Prototype design pattern is used in scenarios where application needs to create a large number of instances of a class, which have almost same state or differ very little. |
+3. *Factory*: Factory design pattern is most suitable when complex object creation steps are involved. To ensure that these steps are centralized and not exposed to composing classes. |
+4. *Abstract factory*: Abstract factory pattern is used whenever we need another level of abstraction over a group of factories created using factory pattern. |
+5. *Singleton*: Singleton enables an application to have one and only one instance of a class per JVM.
+
---
+++ src/com/tts/creational/FactoryMethodDesign.java
@@ -0,0 +1,66 @@
+package com.tts.creational;
+
+// https://www.geeksforgeeks.org/factory-method-design-pattern-in-java/
+
+interface Notification {
+    void notifyUser();
+}
+
+class SMSNotification implements Notification {
+
+    @Override
+    public void notifyUser()
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Sending an SMS notification");
+    }
+}
+
+class EmailNotification implements Notification {
+
+    @Override
+    public void notifyUser()
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Sending an e-mail notification");
+    }
+}
+
+class PushNotification implements Notification {
+
+    @Override
+    public void notifyUser()
+    {
+        // TODO Auto-generated method stub
+        System.out.println("Sending a push notification");
+    }
+}
+
+class NotificationFactory {
+    public Notification createNotification(String channel)
+    {
+        if (channel == null || channel.isEmpty())
+            return null;
+        if ("SMS".equals(channel)) {
+            return new SMSNotification();
+        }
+        else if ("EMAIL".equals(channel)) {
+            return new EmailNotification();
+        }
+        else if ("PUSH".equals(channel)) {
+            return new PushNotification();
+        }
+        return null;
+    }
+}
+
+class NotificationService {
+    public static void main(String[] args)
+    {
+        NotificationFactory notificationFactory = new NotificationFactory();
+        Notification notification = notificationFactory.createNotification("SMS");
+        notification.notifyUser();
+    }
+}
+
+
