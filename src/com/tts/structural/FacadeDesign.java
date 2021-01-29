91a90bd95e208fe49a6485f6d9b8f64c857849b9
sreneewatkins <74524591+sreneewatkins@users.noreply.github.com>
2021-01-28 20:08:18 -0600

    Finished Structural Design Patterns

--- src/com/tts/structural/FacadeDesign.java
+++ src/com/tts/structural/FacadeDesign.java
@@ -1,3 +1,79 @@
 package com.tts.structural;
 
 // https://www.geeksforgeeks.org/facade-design-pattern-introduction/
+/*
+interface Hotel
+{
+    public Menus getMenus();
+}
+
+public class NonVegRestaurant implements Hotel
+{
+    public Menus getMenus()
+    {
+        NonVegMenu nv = new NonVegMenu();
+        return nv;
+    }
+}
+
+public class VegRestaurant implements Hotel
+{
+    public Menus getMenus()
+    {
+        VegMenu v = new VegMenu();
+        return v;
+    }
+}
+
+public class VegNonBothRestaurant implements Hotel
+{
+    public Menus getMenus()
+    {
+        Both b = new Both();
+        return b;
+    }
+}
+
+public class HotelKeeper
+{
+    public VegMenu getVegMenu()
+    {
+        VegRestaurant v = new VegRestaurant();
+        VegMenu vegMenu = (VegMenu)v.getMenus();
+        return vegMenu;
+    }
+
+    public NonVegMenu getNonVegMenu()
+    {
+        NonVegRestaurant v = new NonVegRestaurant();
+        NonVegMenu NonvegMenu = (NonVegMenu)v.getMenus();
+        return NonvegMenu;
+    }
+
+    public Both getVegNonMenu()
+    {
+        VegNonBothRestaurant v = new VegNonBothRestaurant();
+        Both bothMenu = (Both)v.getMenus();
+        return bothMenu;
+    }
+}
+
+public class Client
+{
+    public static void main (String[] args)
+    {
+        HotelKeeper keeper = new HotelKeeper();
+
+        VegMenu v = keeper.getVegMenu();
+        NonVegMenu nv = keeper.getNonVegMenu();
+        Both = keeper.getVegNonMenu();
+
+    }
+}
+
+class VegMenu {}
+class Menu {}
+class NonVegMenu {}
+class Both {}
+
+*/
